9cf2f0fb8daaf666fc91e414b369a62a14947c67
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-27 17:42:40 +0200

    Changes web.php routes order

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any?}', function () {
-    return view('guest.home');
-})->where("any", ".*");
 
 Auth::routes();
 
@@ -28,3 +25,7 @@ Route::middleware('auth')
 
         Route::resource('posts', 'PostController');
     });
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any", ".*");
